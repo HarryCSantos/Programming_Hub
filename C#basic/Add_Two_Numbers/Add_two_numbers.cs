5e2c2ebcc27e8ab624356fe99abe089eb9773315
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-20 07:51:31 -0300

    alt

--- C#basic/Add_Two_Numbers/Add_two_numbers.cs
+++ C#basic/Add_Two_Numbers/Add_two_numbers.cs
@@ -16,7 +16,14 @@ namespace AddTwoNumbers
             number1 =
             int.Parse(Console.ReadLine());
 
-            
+            Console.Write("\nEnter second number:"):
+            number2 =
+            int.Parse(Console.ReadLine());
+
+            //logic of adding two numbers sum=number1 + number2;
+
+            Console.Write("\nSum of the 2 numbers is:"+sum);
+            Console.ReadLine();
         }
     }
 }
