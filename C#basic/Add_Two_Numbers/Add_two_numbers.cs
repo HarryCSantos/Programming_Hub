d22e6fe9a821316ee1cd2591889eaf14277d061a
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-20 07:46:16 -0300

    alt

--- C#basic/Add_Two_Numbers/Add_two_numbers.cs
+++ C#basic/Add_Two_Numbers/Add_two_numbers.cs
@@ -4,3 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+namespace AddTwoNumbers
+{
+    class AddTwoNumbers
+    {
+        static void Main(string[]args)
+        {
+            int number1,number2,sum;
+
+            Console.Write("Enter first number:");
+            number1 =
+            int.Parse(Console.ReadLine());
+
+            
+        }
+    }
+}
