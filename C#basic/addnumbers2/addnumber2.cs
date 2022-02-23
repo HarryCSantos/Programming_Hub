4ca775dc715d4263f8b0d637632f42a29548d611
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-22 22:11:47 -0300

    alt

--- C#basic/addnumbers2/addnumber2.cs
+++ C#basic/addnumbers2/addnumber2.cs
@@ -8,7 +8,7 @@ namespace ArrayListExample
         static void Main(string[] args)
         {
             //creating arraylist
-            ArrayList al= new ArrayList
+            ArrayList al= new ArrayList();
             al.Add("Java");
             al.Add("C");
             al.Add("C++");
