1ea670593a66376562b63617bbf75350f2fb1ea2
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-22 22:06:16 -0300

    alt

--- C#basic/addnumbers2/addnumber2.cs
+++ C#basic/addnumbers2/addnumber2.cs
@@ -7,7 +7,10 @@ namespace ArrayListExample
     {
         static void Main(string[] args)
         {
-            //
+            //creating arraylist
+            ArrayList al= new ArrayList
+            al.Add("Java");
+            al.Add("C");
         }
     }
 }
